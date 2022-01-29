ce3c1dd3aa576a94c230ab695193da5064d0f98b
najmah <najmahmohaaa@gmail.com>
2022-01-29 22:04:54 +0300

    add homepage

---
+++ script.js
@@ -0,0 +1,28 @@
+var btn = document.getElementById("btn")
+var imges = new Array();
+imges[0] = new Image();
+imges[0].src = "imge1.jpg";
+imges[1] = new Image();
+imges[1].src = "imge2.jpeg"
+
+btn.addEventListener("click", function(){
+
+    // var img = document.getElementById("container");
+
+    // for(var i = 0; i < imges.length;i++)
+    // {
+    //     if(imges[i].src == img.src) // << check this
+    //     {
+    //         if(i === imges.length){
+    //             document.getElementById("output").src = imges[0].src;
+    //             break;
+    //         }
+    //         document.getElementById("output").src = imges[i+1].src;
+    //         break;
+    //     }
+    // }
+
+})
+
+
+
